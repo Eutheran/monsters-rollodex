95e28c1ff3751df44d028a3bbb3a75be22eec050
Eric K <kreitere@gmail.com>
2019-10-09 17:23:46 -0400

    Updates

---
+++ precache-manifest.0044ba03b5e3db1763a48c86a84c586d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7b773309a3cc0181029978994efd5e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2bcfd610d54d315eab99",
+    "url": "/monsters-rolodex/static/css/main.057ce2fd.chunk.css"
+  },
+  {
+    "revision": "f0fd004aafba1ae4d4bd",
+    "url": "/monsters-rolodex/static/js/2.702c9599.chunk.js"
+  },
+  {
+    "revision": "2bcfd610d54d315eab99",
+    "url": "/monsters-rolodex/static/js/main.2d536949.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
